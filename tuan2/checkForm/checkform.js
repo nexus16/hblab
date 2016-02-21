5a4dd663a6c342b288d5190c4bfe06b70611712e
lynk <danghoanglinh161994@gmail.com>
2016-02-21 18:47:48 +0700

    2

--- tuan2/checkForm/checkform.js
+++ tuan2/checkForm/checkform.js
@@ -6,61 +6,66 @@ $.fn.checkForm = function(options){
                 max : 40,
                 error : ''
             },options);
-            return $(this).change(function(){
-                
-                var value= $(this).val();
-                var type= $(this).attr('character');
-                var error = $(this).parent().find('p');
-                setting.maxLength = $(this).attr('maxLength');
-                setting.minLength = $(this).attr('minLength');
-                setting.max = $(this).attr('max');
-                setting.min = $(this).attr('min');
+            return $(this).each(function(){
+                var input = $(this).find('input');
+                input.each( function(){
+                    $(this).change( function(){
 
-                console.log(value);
-                console.log('--------------------');
+                        var value= $(this).val();
+                        var type= $(this).attr('character');
+                        var error = $(this).parent().find('p');
+                        setting.maxLength = $(this).attr('maxLength');
+                        setting.minLength = $(this).attr('minLength');
+                        setting.max = $(this).attr('max');
+                        setting.min = $(this).attr('min');
 
-                switch(type)
-                {
-                    case "text":
-                        if(value.length < setting.minLength || value.length > setting.maxLength)
+                        console.log(value);
+                        console.log('--------------------');
+
+                        switch(type)
                         {
-                            setting.error = "nhap tu " + setting.minLength + " den " + setting.maxLength + " ki tu";
-                            error.html(setting.error).css({'color':'red','display':'inline'});
-                        }
-                        else error.html("");
-                        break;
-
-                    case "number":
-                        if(isFinite(value))
-                        {
-                            var value1 = parseFloat(value);
-                            if ( value1 < setting.min || value1 > setting.max )
-                            {
-                                setting.error = "gia tri nhap tu " + setting.min + " den " + setting.max;
-                                error.html(setting.error).css({'color':'red','display':'inline'});
-                            }
-                            else error.html("");
-                        }
-                        else error.html("gia tri nhap la 1 so").css({'color':'red','display':'inline'})
-                        break;
-
-                    case "digits":
-                        if(isFinite(value))
-                        {
-                            var value1 = parseInt(value);
-                            if(value1 == value)
-                            {
-                                if ( value1 < setting.min || value1 > setting.max )
+                            case "text":
+                                if( value.length < setting.minLength || value.length > setting.maxLength )
                                 {
-                                    setting.error = "gia tri nhap tu " + setting.min + " den " + setting.max;
-                                    error.html(setting.error).css({'color':'red','display':'inline'});
+                                    setting.error = "nhap tu " + setting.minLength + " den " + setting.maxLength + " ki tu";
+                                    error.html( setting.error ).css( {'color':'red','display':'inline'} );
                                 }
-                                else error.html("");
-                            }
-                            else error.html( "gia tri nhap la 1 so nguyen" ).css({'color':'red','display':'inline'})
+                                else error.html( "" );
+                                break;
+
+                            case "number":
+                                if( isFinite( value ) )
+                                {
+                                    var value1 = parseFloat(value);
+                                    if ( value1 < setting.min || value1 > setting.max )
+                                    {
+                                        setting.error = "gia tri nhap tu " + setting.min + " den " + setting.max;
+                                        error.html( setting.error ).css( {'color':'red','display':'inline'} );
+                                    }
+                                    else error.html("");
+                                }
+                                else error.html( "gia tri nhap la 1 so" ).css( {'color':'red','display':'inline'} )
+                                break;
+
+                            case "digits":
+                                if(isFinite(value))
+                                {
+                                    var value1 = parseInt(value);
+                                    if(value1 == value)
+                                    {
+                                        if ( value1 < setting.min || value1 > setting.max )
+                                        {
+                                            setting.error = "gia tri nhap tu " + setting.min + " den " + setting.max;
+                                            error.html(setting.error).css( {'color':'red','display':'inline'} );
+                                        }
+                                        else error.html("");
+                                    }
+                                    else error.html( "gia tri nhap la 1 so nguyen" ).css({'color':'red','display':'inline'})
+                                }
+                                else error.html( "gia tri nhap la 1 so nguyen" ).css({'color':'red','display':'inline'})
+                                break;
                         }
-                        else error.html( "gia tri nhap la 1 so nguyen" ).css({'color':'red','display':'inline'})
-                        break;
-                }
+                    })
+                });         
             })
         }
